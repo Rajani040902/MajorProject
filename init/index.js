ee8c343f88c27e42ce1c9bbe3fcd381ef4885c40
Rajani <rjha38221@gmail.com>
2026-06-21 17:05:45 +0530

    Fix seed file for Atlas

--- init/index.js
+++ init/index.js
@@ -1,12 +1,10 @@
-if (process.env.NODE_ENV != "production") {
-    require('dotenv').config({ path: '../.env' });
-}
+require('dotenv').config(); // ✅ remove path option
 
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
 .then(() => {
