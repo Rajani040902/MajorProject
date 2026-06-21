b19a8e92a9518b5c248c3d1a6a748c96622137ce
Rajani <rjha38221@gmail.com>
2026-06-21 16:20:00 +0530

    Fix root redirect, currUser, init file

--- init/index.js
+++ init/index.js
@@ -1,8 +1,12 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config({ path: '../.env' });
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
 .then(() => {
@@ -13,14 +17,17 @@ main()
 });
 
 async function main() {
-     await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
     await Listing.deleteMany({});
-   initData.data= initData.data.map((obj) => ({...obj, owner: "6a1b046d3d8e7b138a231151"}));
+    initData.data = initData.data.map((obj) => ({
+        ...obj, 
+        owner: "6a2d5c746d56779fffdf1292"
+    }));
     await Listing.insertMany(initData.data);
-    console.log("data was initialize");
+    console.log("data was initialized");
 };
 
 initDB();
